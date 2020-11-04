cea0a8f58877aa39cab7b17fb18e92b5f59b050f
Suvankar <manashisuvankar@gmail.com>
2020-11-04 21:29:36 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {} from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter,Route,Link,Switch,Router} from "react-router-dom"
+import Navbar from './layouts/Navbar.js'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
     </div>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/layouts/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {Link}  from "react-router-dom"
+import logo from './logo.gif'; 
+ class Navbar extends Component {
+    render() {
+        return (
+            
+                <nav className="navbar navbar-expand-lg navbar-dark full-dark">
+  <button className="navbar-toggler" type="button" data-toggle="collapse"
+   data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <Link className="navbar-brand" to="/"><img width="36" height="36" src={logo} alt=""></img>Agile Tech</Link>
+
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/">About<span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/">Contact<span className="sr-only">(current)</span></Link>
+      </li>
+    </ul>
+  </div>
+</nav>
+           
+        )
+    }
+}
+
+export default Navbar
